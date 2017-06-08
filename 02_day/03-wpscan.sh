42d6a99565a0328e029363a6e63f130809a91cfc
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-08 13:09:20 -0300

    Create 03-wpscan.sh

--- 02_day/03-wpscan.sh
+++ 02_day/03-wpscan.sh
@@ -5,11 +5,13 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 31/10/2016
-# Data de atualização: 31/10/2016
-# Versão: 1.0
+# Data de atualização: 05/06/2017
+# Versão: 1.1
+
+#Comandos utilizados no #Escape_Room_TI_SENAC_Tatuapé_30_Anos
 
 #Variável do comando WPScan
-ALVO="www.SEU_ALVO.br"
+ALVO="www.uninove.br"
 
 #Executando o comando WPScan sem opções. 
 echo -e "Executando o comando WPScan no alvo: $ALVO"
