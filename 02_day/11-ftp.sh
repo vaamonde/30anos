9380443244ed11787ade00d069497b31e7f6e175
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-08 13:16:24 -0300

    Create 11-ftp.sh

--- 02_day/11-ftp.sh
+++ 02_day/11-ftp.sh
@@ -5,17 +5,20 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 31/10/2016
-# Data de atualização: 31/10/2016
-# Versão: 1.0
+# Data de atualização: 05/06/2017
+# Versão: 1.1
+
+#Comandos utilizados no #Escape_Room_TI_SENAC_Tatuapé_30_Anos
 
 #Variável do comando Hydra
-ALVO="www.SEU_ALVO.br"
+ALVO="www.uninove.br"
 USERNAME="usuarios.txt"
 WORDLIST="senhas.txt"
 PORT="21"
 SERVICE="ftp"
 
-#Executando o comando Hydra com a opções: -s=Porta, -e=Checagens Adcionais, -f=Após confirmação de Login, sair do acesso, -vV=Modo Verboso, detalhado na hora de passar usuário e senhas, -L=Wordlist de Usuários, -P=Wordlist de senhas, -t=Tarefas/Tentativas
+#Executando o comando Hydra com a opções: -s=Porta, -e=Checagens Adcionais, -f=Após confirmação de Login, sair do acesso, 
+#-vV=Modo Verboso, detalhado na hora de passar usuário e senhas, -L=Wordlist de Usuários, -P=Wordlist de senhas, -t=Tarefas/Tentativas
 #Informações levantadas utilizando o comando Nmap
 echo -e "Executando o comando Hydra no alvo: $ALVO"
 echo
