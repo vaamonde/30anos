6e949f2d1f1fad5b7b22256fea319ce32290eaa0
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-08 13:15:26 -0300

    Create 09-sucrack.sh

--- 02_day/09-sucrack.sh
+++ 02_day/09-sucrack.sh
@@ -5,11 +5,13 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 31/10/2016
-# Data de atualização: 31/10/2016
-# Versão: 1.0
+# Data de atualização: 05/06/2017
+# Versão: 1.1
+
+#Comandos utilizados no #Escape_Room_TI_SENAC_Tatuapé_30_Anos
 
 #Variável do comando 
-ALVO="www.SEU_ALVO.br"
+ALVO="www.uninove.br"
 USERNAME="SEU_USUARIO"
 
 #Executando o comando scp e ssh sem opções: 
