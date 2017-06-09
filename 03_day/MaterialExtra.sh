d65c5320bb15b8dcf2be105d08f122db2b7dbe91
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-09 19:32:05 -0300

    Create MaterialExtra.sh

---
+++ 03_day/MaterialExtra.sh
@@ -0,0 +1,9 @@
+#Material extra.
+
+Canal no YouTUBE: https://www.youtube.com/boraparapratica
+
+Facebook Procedimentos em TI: https://www.facebook.com/ProcedimentosEmTi/
+
+Facebook Bora para Prática!!: https://www.facebook.com/boraparapratica/
+
+Mega.nz Procedimentos em TI: https://mega.nz/#F!Co9GHIyK!2kzNnN7XzImP01M1SyRm2g
