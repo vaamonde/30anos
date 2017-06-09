028baa620ee89b35f5e19c64ad232833858bd5d5
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-06-09 10:22:53 -0300

    Create FinalizeSuaMissao.sh

---
+++ 03_day/FinalizeSuaMissao.sh
@@ -0,0 +1,13 @@
+#Finalize sua Missão!!!!!
+
+Endereço para sua missão: http://??????.rhcloud.com/
+
+#Objetivo Final:
+Acessar o WP-Admin do Wordpress, fazer uma pichação em qualquer página com o seu Nome, Data, Hora.
+
+#Técnicas para Invasão:
+Pode utilizar todas as técnicas que você aprendeu nos dois dias do evento.
+
+#BoaSortePessoal!!!!!
+
+Robson Vaamonde
